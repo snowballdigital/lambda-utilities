2b432f3104fe0d100f8de8ea624cf2bd6a91546c
Michael Smesnik <michael@snowball.digital>
2018-09-18 13:11:14 +0200

    prototype implementation (yes, i will add tests later)

---
+++ index.js
@@ -0,0 +1,40 @@
+const normalizeRequestParameters = ({ headers, path, pathParameters, httpMethod, queryStringParameters, body }) => {
+  const standardizedHeaders = Object.entries(headers).reduce((acc, [ headerName, value ]) => {
+    return { ...acc, [ headerName.toLowerCase() ]: value }
+  }, { })
+
+  const isJson = standardizedHeaders['content-type'] && standardizedHeaders['content-type'].toLowerCase() === 'application/json'
+
+  return {
+    headers: standardizedHeaders,
+    query: queryStringParameters || { },
+    path,
+    method: httpMethod,
+    params: pathParameters || { },
+    body: body && isJson ? JSON.parse(body) : body
+  }
+}
+
+const normalizeRequestHandler = (func) => async (event, context) => {
+  let statusCode
+  let body
+
+  try {
+    body = await func(normalizeRequestParameters(event), event, context)
+
+    statusCode = typeof body !== 'undefined' ? 200 : 204
+  } catch (e) {
+    statusCode = e.statusCode || e.status || e.code || 500
+    body = { message: e.body || e.message || 'Internal server error' }
+  }
+
+  return {
+    statusCode,
+    body: JSON.stringify(body)
+  }
+}
+
+module.exports = {
+  normalizeRequestParameters,
+  normalizeRequestHandler
+}
